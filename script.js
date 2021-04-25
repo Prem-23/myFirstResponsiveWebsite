c6fd3665be856d8277517a6ae6c8cbbe1ff87fac
Prem-23 <71540670+Prem-23@users.noreply.github.com>
2021-04-25 03:21:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ let list = document.querySelectorAll('ul li a');
 let toggle = document.querySelector('#toggle');
 let header = document.getElementById('head');
 let navBar = document.getElementById('navbar');
-
-list.forEach(item => {
-    item.addEventListener('click', function() {
-        let activeNav = document.querySelector('.home');
-        activeNav.className = activeNav.className.replace('home', '');
-        item.className += 'home';
-    })
-})
-
+let icon = document.querySelector('.fa');
+let icon1 = document.querySelector('.arrow .fa');
+let arrow = document.querySelector('.arrow');
+let body = document.querySelector('body');
 
 document.onclick = function(e) {
     if (e.target.id !== 'navBar' && e.target.id !== 'toggle') {
@@ -23,3 +18,23 @@ toggle.onclick = function() {
     toggle.classList.toggle('active');
     navBar.classList.toggle('active');
 }
+
+icon.addEventListener('click', function() {
+    if (icon.className == 'fa fa-moon-o') {
+        icon.className = icon.className.replace('fa-moon-o', 'fa-sun-o');
+        body.classList.add('darkLight');
+    } else {
+        icon.className = icon.className.replace('fa-sun-o', 'fa-moon-o');
+        body.classList.remove('darkLight');
+    }
+})
+
+icon1.addEventListener('click', function() {
+    if (icon1.className == 'fa fa-angle-double-down') {
+        icon1.className = icon1.className.replace('fa-angle-double-down', 'fa-angle-double-up');
+        arrow.classList.add('up');
+    } else {
+        icon1.className = icon1.className.replace('fa-angle-double-up', 'fa-angle-double-down');
+        arrow.classList.remove('up');
+    }
+})
