b56933d4569c1993dc033e2ef3d54a14539791b8
Prem-23 <71540670+Prem-23@users.noreply.github.com>
2021-04-27 04:49:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-let list = document.querySelectorAll('ul li a');
-let toggle = document.querySelector('#toggle');
-let header = document.getElementById('head');
-let navBar = document.getElementById('navbar');
-let icon = document.querySelector('.fa');
-let icon1 = document.querySelector('.arrow .fa');
-let arrow = document.querySelector('.arrow');
+// to select active Screen
+
+let activeScreen = document.querySelectorAll('ul li a');
+activeScreen.forEach(item => {
+    item.addEventListener('click', function() {
+        home = document.querySelector('.active');
+        home.className = home.className.replace('active', '');
+        item.className += 'active';
+    })
+})
+
+// moon to sun
+
+let icon = document.querySelector('.moon');
 let body = document.querySelector('body');
-let hyperRef = document.querySelector('a[href = "#foot"]');
-
-document.onclick = function(e) {
-    if (e.target.id !== 'navBar' && e.target.id !== 'toggle') {
-        navBar.classList.remove('active');
-        toggle.classList.remove('active');
-    }
-}
-
-toggle.onclick = function() {
-    toggle.classList.toggle('active');
-    navBar.classList.toggle('active');
-}
-
 icon.addEventListener('click', function() {
-    if (icon.className == 'fa fa-moon-o') {
+    if (icon.className == 'fa fa-moon-o moon') {
         icon.className = icon.className.replace('fa-moon-o', 'fa-sun-o');
         body.classList.add('darkLight');
     } else {
         icon.className = icon.className.replace('fa-sun-o', 'fa-moon-o');
         body.classList.remove('darkLight');
+
     }
 })
 
+// angle up and down
+
+let icon1 = document.querySelector('.angle');
+let hyperRef = document.querySelector('a[href = "#foot"]');
 icon1.addEventListener('click', function() {
-    if ((icon1.className == 'fa fa-angle-double-down')) {
+    if (icon1.className == 'fa fa-angle-double-down angle') {
         icon1.className = icon1.className.replace('fa-angle-double-down', 'fa-angle-double-up');
-        arrow.classList.add('up');
         hyperRef.setAttribute('href', '#foot');
     } else {
         icon1.className = icon1.className.replace('fa-angle-double-up', 'fa-angle-double-down');
-        arrow.classList.remove('up');
         hyperRef.setAttribute('href', '#home');
     }
 })
